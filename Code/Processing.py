75fa8eb280a7b108e2add79ea361eb82d9321cbf
GameCat6 <75439296+GameCat6@users.noreply.github.com>
2022-11-07 18:48:02 +0500

    Update Processing.py

--- Code/Processing.py
+++ Code/Processing.py
@@ -9,23 +9,28 @@ from tensorflow.keras.preprocessing import image_dataset_from_directory
 from tensorflow.keras.preprocessing import image
 import os
 def pred1(path,model):
-    c1 = list()
     img_size = 150
     for file in os.listdir(path):
+        # c1.append(file)
         path1 =  path + file 
-        img = image.load_img(path1, target_size=(img_size, img_size))
-        img_array = image.img_to_array(img)
-        img_array = np.expand_dims(img_array, axis=0)
-        path2 = '"' + 'C:\Python Programs\PandaCat1\Test\Test' + '\\' + file + '"'
-        if np.argmax(model.predict(img_array,verbose=0)) == 0:
-            respath = 'copy ' + path2 + ' "C:\Python Programs\PandaCat1\Result\Cats\"'
-        else:
-            if np.argmax(model.predict(img_array,verbose=0)) == 1:
-                respath = 'copy ' + path2 + ' "C:\Python Programs\PandaCat1\Result\Dogs\"'
+        try:
+            img = image.load_img(path1, target_size=(img_size, img_size))    
+            img_array = image.img_to_array(img)
+            img_array = np.expand_dims(img_array, axis=0)
+            # print(file,':')
+            # print(np.argmax(model.predict(img_array,verbose=0)))
+            path2 = '"' + 'C:\Python Programs\PandaCat1\Test\Test' + '\\' + file + '"'
+            if np.argmax(model.predict(img_array,verbose=0)) == 0:
+                respath = 'copy ' + path2 + ' "C:\Python Programs\PandaCat1\Result\Cats\"'
             else:
-                respath = 'copy ' +  path2 + ' "C:\Python Programs\PandaCat1\Result\Pandas\"'
-        os.system(respath)
-        print(respath)
+                if np.argmax(model.predict(img_array,verbose=0)) == 1:
+                    respath = 'copy ' + path2 + ' "C:\Python Programs\PandaCat1\Result\Dogs\"'
+                else:
+                    respath = 'copy ' +  path2 + ' "C:\Python Programs\PandaCat1\Result\Pandas\"'
+            os.system(respath)
+            print(respath)
+        except OSError:
+    	    pass
 model = keras.models.load_model('SavedAI/my_model')
 test_dir = 'C:/Python Programs/PandaCat1/Test/Test/'
 print(pred1(test_dir,model))
